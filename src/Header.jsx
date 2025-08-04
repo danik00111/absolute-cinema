7a267db0e585bb3a9edd5e20ec2cb50168b7b18c
danik00111 <kremenskiydaniil@gmail.com>
2025-08-04 12:38:16 +0300

    aganin

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Header =()=> (
   <header style={{width:'fit-content',margin:'0 auto'}}>
-    <Link style={{margin:'0 8px'}} to='/absoulute-cinema/'>Bottom Text Studios</Link>
-    <Link style={{margin:'0 8px'}} to='/absoulute-cinema/movies'>Movie List</Link>
+    <Link style={{margin:'0 8px'}} to='/absolute-cinema'>Bottom Text Studios</Link>
+    <Link style={{margin:'0 8px'}} to='/absolute-cinema/movies'>Movie List</Link>
   </header>
 ); export default Header;
